8c697b491ccbb198a9519c845fa2e75d53825df8
Toby Buckley <tobuck@amazon.com>
2022-08-23 09:37:40 -0700

    remove package-locks and updated deps

--- scripts/bootstrap/postinstall.sh
+++ scripts/bootstrap/postinstall.sh
@@ -3,12 +3,12 @@
 # SPDX-License-Identifier: MIT
 
 
-(cd resources/add_federation_configuration_lambda && npm install --only=prod)
-(cd resources/add_ldap_user_group && npm install --only=prod)
-(cd resources/add_tenant_federation_lambda_authorizer && npm install --only=prod)
-(cd resources/add_tenant_infra_lambda && npm install --only=prod)
-(cd resources/finish_oidc_provider_pipeline_lambda && npm install --only=prod)
-(cd resources/oidc-client/oidc_client_function && npm install --only=prod)
-(cd resources/oidc-provider && npm install --only=prod)
-(cd resources/oidc-resource/authorizer && npm install --only=prod)
-(cd resources/start_oidc_provider_pipeline_lambda && npm install --only=prod)
+(cd resources/add_federation_configuration_lambda && yarn install --only=prod)
+(cd resources/add_ldap_user_group && yarn install --only=prod)
+(cd resources/add_tenant_federation_lambda_authorizer && yarn install --only=prod)
+(cd resources/add_tenant_infra_lambda && yarn install --only=prod)
+(cd resources/finish_oidc_provider_pipeline_lambda && yarn install --only=prod)
+(cd resources/oidc-client/oidc_client_function && yarn install --only=prod)
+(cd resources/oidc-provider && yarn install --only=prod)
+(cd resources/oidc-resource/authorizer && yarn install --only=prod)
+(cd resources/start_oidc_provider_pipeline_lambda && yarn install --only=prod)
